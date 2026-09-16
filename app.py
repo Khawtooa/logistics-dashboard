fcc2f0af25482f9cae756ac9c882b1e2b8ecc442
Khawtooa <164462206+Khawtooa@users.noreply.github.com>
2026-09-16 12:41:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ st.markdown("""
 @st.cache_data
 def load_data():
     file_path = "Delivery_Logistics_300rows_12cols.xlsx"
-    df = pd.read_excel(file_path, sheet_name="Logistics_Data_300")
+    df = pd.read_excel(file_path, df = pd.read_excel(file_path)
     return df
 
 df = load_data()
